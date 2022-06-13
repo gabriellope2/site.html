aa6bffdc1a1fac7b8586aa8421e6a19ec697cd19
gabriel da silva lopes <gabrieldasilvalopes@ucl.br>
2022-06-13 19:34:28 -0300

    acertos
    acertos para hospedagem

--- imagens/script.js
+++
@@ -1,4 +0,0 @@
-<script>  
-
-document.write("hello word")
- </script>
---
+++ javaScript/verificarForm.js
@@ -0,0 +1,31 @@
+function mudar(){
+
+        window.location.href = "index.html"
+   }
+
+
+ function validarCPF() {	
+     var cpf =  document.getElementById("cpf")
+	if(cpf == '') return false;	
+
+			
+
+	add = 0;	
+	for (i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev == 10 || rev == 11)		
+			rev = 0;	
+		if (rev != parseInt(cpf.charAt(9)))		
+			return false;		
+
+	add = 0;	
+	for (i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev == 10 || rev == 11)	
+		rev = 0;	
+	if (rev != parseInt(cpf.charAt(10)))
+		return false;		
+	return true;   
+}
